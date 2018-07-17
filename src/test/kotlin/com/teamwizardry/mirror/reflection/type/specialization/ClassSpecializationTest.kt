e4211bc8540ee9b9ccc6c1c85beae07e8823afc5
Pierce Corcoran <pierce@plasticcow.com>
2018-07-17 14:09:29 -0500

    Finished superclass testing

--- src/test/kotlin/com/teamwizardry/mirror/reflection/type/specialization/ClassSpecializationTest.kt
+++ src/test/kotlin/com/teamwizardry/mirror/reflection/type/specialization/ClassSpecializationTest.kt
@@ -6,12 +6,14 @@ import com.teamwizardry.mirror.reflection.type.Object1
 import com.teamwizardry.mirror.reflection.type.TypeMirror
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNotEquals
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
 internal class ClassSpecializationTest: MirrorTestBase() {
     @Test
-    fun classSpecialize_onSimpleGeneric_shouldReturnTypeWithNewTypeParametersAndRaw() {
+    @DisplayName("Specializing generic type should return mirror with new type parameters and correct value for raw")
+    fun basicSpecialization() {
         class SimpleGeneric<T>
         val genericType = Mirror.reflectClass(SimpleGeneric::class.java)
         val specializeWith = Mirror.reflectClass<Object1>()
@@ -23,7 +25,8 @@ internal class ClassSpecializationTest: MirrorTestBase() {
     }
 
     @Test
-    fun classSpecialize_withIncorrectParameterCount_shouldThrowIllegalArgumentException() {
+    @DisplayName("Specializing type with wrong number of arguments should throw IllegalArgumentException")
+    fun specializingWrongParameterCount() {
         class SimpleGeneric<T>
         val genericType = Mirror.reflectClass(SimpleGeneric::class.java)
         val specializeWith = Mirror.reflectClass<Object1>()
@@ -32,8 +35,12 @@ internal class ClassSpecializationTest: MirrorTestBase() {
         }
     }
 
+    //=================================================== Supertype ====================================================
+
     @Test
-    fun reflect_withSpecializedSupertype_shouldReturnTypeWithSpecializedSupertype() {
+    @DisplayName("`T1 extends T2<SomeType>; T2<A>`: Reflecting T1 should return a mirror whose supertype is T2 " +
+            "specialized for SomeType")
+    fun explicitSupertypeSpecialization() {
         open class SimpleGeneric<T>
         class SimpleSubtype: SimpleGeneric<String>()
         val simpleType = Mirror.reflectClass(SimpleSubtype::class.java)
@@ -44,7 +51,9 @@ internal class ClassSpecializationTest: MirrorTestBase() {
     }
 
     @Test
-    fun reflect_withTypePassingVariableUpToSupertype_shouldReturnTypeWithSupertypeSpecializedWithVariable() {
+    @DisplayName("`T1<A> extends T2<A>; T2<B>`: Reflecting T1 should return a mirror whose supertype is T2 " +
+            "specialized for A")
+    fun reflectedHandoffToSupertype() {
         open class SimpleGeneric<T>
         class SimpleSubtype<A>: SimpleGeneric<A>()
         val simpleType = Mirror.reflectClass(SimpleSubtype::class.java)
@@ -55,12 +64,46 @@ internal class ClassSpecializationTest: MirrorTestBase() {
     }
 
     @Test
-    fun classSpecialize_withTypePassingVariableUpToSupertype_shouldReturnTypeWithSupertypeSpecializedToNewParameter() {
+    @DisplayName("`T1<A> extends T2<A>; T2<B>`: Specializing T1 for SomeType should return a mirror whose supertype " +
+            "is specialized for SomeType")
+    fun specializedHandoffToSupertype() {
         open class SimpleGeneric<T>
         class SimpleSubtype<A>: SimpleGeneric<A>()
         val specialized = Mirror.reflectClass(SimpleSubtype::class.java).specialize(Mirror.reflect<String>())
         val supertype = specialized.superclass!!
 
         assertEquals(listOf(Mirror.reflect<String>()), supertype.typeParameters)
+    }
+
+    @Test
+    @DisplayName("`T1<A> extends T2<T3<A>>; T2<B>; T3<C>`: Reflecting T1 should return mirror whose supertype is " +
+            "specialized for T3<A>")
+    fun reflectedHandoffWrappedToSupertype() {
+        open class SimpleGeneric<T>
+        class IntermediateGeneric<T>
+        class SimpleSubtype<A>: SimpleGeneric<IntermediateGeneric<A>>()
+        val subtype = Mirror.reflectClass(SimpleSubtype::class.java)
+        val supertype = subtype.superclass!!
+
+        assertEquals(listOf(
+                Mirror.reflectClass(IntermediateGeneric::class.java)
+                        .specialize(subtype.typeParameters[0])
+        ), supertype.typeParameters)
+    }
+
+    @Test
+    @DisplayName("`T1<A> extends T2<T3<A>>; T2<B>; T3<C>`: Specializing T1 for SomeType should return mirror whose " +
+            "supertype is specialized for T3<SomeType>")
+    fun specializeHandoffWrappedToSupertype() {
+        open class SimpleGeneric<T>
+        class IntermediateGeneric<T>
+        class SimpleSubtype<A>: SimpleGeneric<IntermediateGeneric<A>>()
+        val subtype = Mirror.reflectClass(SimpleSubtype::class.java).specialize(Mirror.reflect<String>())
+        val supertype = subtype.superclass!!
+
+        assertEquals(listOf(
+                Mirror.reflectClass(IntermediateGeneric::class.java)
+                        .specialize(Mirror.reflect<String>())
+        ), supertype.typeParameters)
     }
 }
